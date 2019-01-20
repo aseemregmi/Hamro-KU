fe6ea6d60e33160ec1abbfbeaa653f02f799fb9e
Aseem Regmi <aseem.regmi22@gmail.com>
2019-01-20 20:30:31 +0545

    Added Build Files

---
+++ client/build/precache-manifest.d8c985c75fa36e5b83b065df448e7499.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "39bddf65983f86f26a95514db8d4cb68",
+    "url": "/static/media/KU.39bddf65.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "00694dd4301f2828e4f9",
+    "url": "/static/js/main.00694dd4.chunk.js"
+  },
+  {
+    "revision": "556293cccd1b8092ed72",
+    "url": "/static/js/1.556293cc.chunk.js"
+  },
+  {
+    "revision": "00694dd4301f2828e4f9",
+    "url": "/static/css/main.6dd0a60e.chunk.css"
+  },
+  {
+    "revision": "cdf75d96679b04e152fdab59e6d0ae99",
+    "url": "/index.html"
+  }
+];
